f5153d9a3b22ba76aa6d1f4898c886b11f301f1a
Unas Sikandar <unassikandar@gmail.com>
2024-10-03 17:29:28 +0300

    Update get.go

--- cmd/get.go
+++ cmd/get.go
@@ -12,7 +12,7 @@ import (
 
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
+	Short: "Gets the required todo list",
 	Long: ``,
   Args: cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
